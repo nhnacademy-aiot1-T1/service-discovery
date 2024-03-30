9fdf02b0508f4d38ec8cc025c206d93ba3ad226c
ckddms6530 <ckddms6530@gmail.com>
2024-03-30 15:04:44 +0900

    feat : 유레카 서버 기본 구현

---
+++ src/test/java/live/t1/eureka/EurekaApplicationTests.java
@@ -0,0 +1,15 @@
+package live.t1.eureka;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertDoesNotThrow(() -> {});
+	}
+
+}
